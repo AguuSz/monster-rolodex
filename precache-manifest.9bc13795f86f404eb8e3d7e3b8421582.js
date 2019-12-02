f9601874b6372c623ce25c0062fb3c630aefff05
AguuSz <agus.sepu2000@hotmail.com>
2019-12-02 08:28:02 -0300

    Updates

---
+++ precache-manifest.9bc13795f86f404eb8e3d7e3b8421582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ac88ccd3cd0acc92f2e9d982963488",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4493af2fbbd142835002",
+    "url": "/monster-rolodex/static/css/main.bef8fb8a.chunk.css"
+  },
+  {
+    "revision": "b57582b106bce201a1b0",
+    "url": "/monster-rolodex/static/js/2.b1a4e073.chunk.js"
+  },
+  {
+    "revision": "4493af2fbbd142835002",
+    "url": "/monster-rolodex/static/js/main.8474a953.chunk.js"
+  },
+  {
+    "revision": "427b8cbadf4f13fb47d5",
+    "url": "/monster-rolodex/static/js/runtime-main.4b86bd12.js"
+  }
+]);
